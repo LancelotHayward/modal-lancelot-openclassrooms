3f92c69f1a1b2ca177361142398925672e47073a
LancelotHayward <hayward.lance@gmail.com>
2024-08-17 15:45:39 +0200

    Added JSDoc comments

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,11 @@
 import "./Modal.scss"
 
+
+/**
+ * React component which displays a dialog element.
+ * @param {React.Props} param0 Property "text": Displayed text. Property "reference": React useReference hook from parent.
+ * @returns {React.ReactElement} <Modal/> component.
+ */
 function Modal({text, reference}) {
     return (
         <dialog ref={reference} onClick={e => reference.current.close()} className="modal">
